fccb47743337cf91ffa6dc7850fadaca975658e5
garato <matteo.garato@euronovate.com>
2018-03-07 18:57:21 +0100

    [ADD] - Garato: added configuration and password read

--- bulkMailSender.py
+++ bulkMailSender.py
@@ -1,20 +1,33 @@
 import smtplib
 import configparser
 import logging
-
+import time
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 def main():
     try:
         configParser = configparser.RawConfigParser()
         configFilePath = r'bulkMailSender.config'
         configParser.read(configFilePath)
-        server = smtplib.SMTP(configParser.get('SMTP', 'smtpAddress'), configParser.getint('SMTP', 'smtpPort'))
+        smtpAddress='{}:{}'.format(configParser.get('SMTP', 'smtpAddress'),configParser.getint('SMTP', 'smtpPort'))
+        server = smtplib.SMTP(smtpAddress)
+        server.starttls()
         senderaddress = configParser.get('SMTP', 'senderAddress')
-        server.login(senderaddress, "password")
-        msg = configParser.get('MAIL', 'body')
+        password=raw_input("inserire la password per l'indirizzo {senderAddres}: ".format(senderAddres=senderaddress))
+        server.login(senderaddress, password)
+        msgText = configParser.get('MAIL', 'body')
         addresses = configParser.get('MAIL', 'addresses').split('\n')
         for address in addresses:
-            server.sendmail(senderaddress, address, msg.format(address))
+            companyName=address.split('@')[1]
+            msgText=msgText.format(company=companyName)
+            msg = MIMEMultipart()
+            msg['From'] = senderaddress
+            msg['To'] = address
+            msg.attach(MIMEText(msgText, 'plain'))
+            msg['Subject'] = configParser.get('MAIL', 'object')
+            server.sendmail(senderaddress, address, msg.as_string())
+            time.sleep(configParser.getint('SMTP', 'mailBreak'))
     except Exception as e:
         logging.exception('ERROR: ' + str(e))
 
