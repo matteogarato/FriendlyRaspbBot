595e8e1aa0048cc8eea570554e6f30489e2ba686
matteo <garato.omicron@gmail.com>
2019-01-03 21:16:03 +0100

    [ADD] - Added a call to python script for ini

--- updater.sh
+++ updater.sh
@@ -3,5 +3,6 @@ PIDFile="run.pid"
 sudo kill -9 $(<"$PIDFile")
 sudo cd TelegramBot.config TelegramBotBACKUP.config
 sudo git pull
+sudo python3.5 _iniConverter.py
 sudo rm "run.pid"
 nohup python3.5 TelegramBot.py > /dev/null 2>&1 & echo $! > run.pid
