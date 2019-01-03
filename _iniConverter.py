45947dc12f66e139bec2eef02391c37933817991
matteo <garato.omicron@gmail.com>
2019-01-03 21:12:29 +0100

    [ADD] - Added a ini file converter

---
+++ _iniConverter.py
@@ -0,0 +1,25 @@
+import configparser
+
+
+def main():
+   configParser = configparser.RawConfigParser()
+   configFilePath = r'TelegramBot.config'
+   configParser.read(configFilePath)
+   backupConfig== configparser.RawConfigParser()
+   backupConfigPath = r'TelegramBotBACKUP.config'
+   backupConfig.read(backupConfigPath)
+   if not configParser['BOTCONFIG']['botId']:
+      configParser['BOTCONFIG']['botId']=backupConfig['BOTCONFIG']['botId']
+   if not configParser['BOTCONFIG']['urls']:
+      configParser['BOTCONFIG']['urls']=backupConfig['BOTCONFIG']['urls']
+   if not configParser['BOTCONFIG']['insults']:
+      configParser['BOTCONFIG']['insults']=backupConfig['BOTCONFIG']['insults']
+   if not configParser['BOTCONFIG']['noToInsult']:
+      configParser['BOTCONFIG']['noToInsult']=backupConfig['BOTCONFIG']['noToInsult']
+   if not configParser['BOTCONFIG']['speedtestUrl']:
+      configParser['BOTCONFIG']['speedtestUrl']=backupConfig['BOTCONFIG']['speedtestUrl']
+
+
+if __name__ == '__main__':
+    main()
+
--- updater.sh
+++ updater.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 PIDFile="run.pid"
 sudo kill -9 $(<"$PIDFile")
+sudo cd TelegramBot.config TelegramBotBACKUP.config
 sudo git pull
-suodo rm "run.pid"
+sudo rm "run.pid"
 nohup python3.5 TelegramBot.py > /dev/null 2>&1 & echo $! > run.pid
