1be7acdf86923bf8ee02c7679443a1def19edc33
matteo <garato.omicron@gmail.com>
2019-03-24 17:00:47 +0100

    [ADD] - Added configuration

--- _iniConverter.py
+++ _iniConverter.py
@@ -5,21 +5,22 @@ def main():
    configParser = configparser.RawConfigParser()
    configFilePath = r'TelegramBot.config'
    configParser.read(configFilePath)
-   backupConfig== configparser.RawConfigParser()
+   backupConfig == configparser.RawConfigParser()
    backupConfigPath = r'TelegramBotBACKUP.config'
    backupConfig.read(backupConfigPath)
    if not configParser['BOTCONFIG']['botId']:
-      configParser['BOTCONFIG']['botId']=backupConfig['BOTCONFIG']['botId']
+      configParser['BOTCONFIG']['botId'] = backupConfig['BOTCONFIG']['botId']
    if not configParser['BOTCONFIG']['urls']:
-      configParser['BOTCONFIG']['urls']=backupConfig['BOTCONFIG']['urls']
+      configParser['BOTCONFIG']['urls'] = backupConfig['BOTCONFIG']['urls']
+   if not configParser['BOTCONFIG']['urlsHighway']:
+      configParser['BOTCONFIG']['urlsHighway'] = backupConfig['BOTCONFIG']['urlsHighway']
    if not configParser['BOTCONFIG']['insults']:
-      configParser['BOTCONFIG']['insults']=backupConfig['BOTCONFIG']['insults']
+      configParser['BOTCONFIG']['insults'] = backupConfig['BOTCONFIG']['insults']
    if not configParser['BOTCONFIG']['noToInsult']:
-      configParser['BOTCONFIG']['noToInsult']=backupConfig['BOTCONFIG']['noToInsult']
+      configParser['BOTCONFIG']['noToInsult'] = backupConfig['BOTCONFIG']['noToInsult']
    if not configParser['BOTCONFIG']['speedtestUrl']:
-      configParser['BOTCONFIG']['speedtestUrl']=backupConfig['BOTCONFIG']['speedtestUrl']
+      configParser['BOTCONFIG']['speedtestUrl'] = backupConfig['BOTCONFIG']['speedtestUrl']
 
 
 if __name__ == '__main__':
     main()
-
